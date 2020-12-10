233c13442758b79855df8f4b8fb49f6dee663622
ricsoh <ricsoh@gmail.com>
2020-12-10 17:29:17 +0800

    create flatlist with sample data

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
 import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import { Ionicons } from "@expo/vector-icons";
 import * as SQLite from "expo-sqlite";
 
+// Open database if exists else create
 const db = SQLite.openDatabase("notes.db");
 
 function NotesScreen({ navigation }) {
+
+  const [notes, setNotes] = useState([
+    { title: "Walk the cat", done: false, id: "0" },
+    { title: "Feed the elephant", done: false, id: "1" },
+  ]);
+ 
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -27,9 +34,38 @@ function NotesScreen({ navigation }) {
     });
    });
 
+   function addNote() {
+    let newNote = {
+      title: "Sample new note",
+      done: false,
+      id: notes.length.toString(),
+    };
+    setNotes([...notes,]);
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+
  return (
   <View style={styles.container}>
-    <Text>Home Screen</Text>
+     <FlatList
+       style={{ width: "100%" }}
+       data={notes}
+       renderItem={renderItem}
+     />
   </View>
  );
 }
